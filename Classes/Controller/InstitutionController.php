c6799bd140ffb53f3e50f4a047c23c159ca70318
Kay Strobach <info@kay-strobach.de>
2020-08-13 10:44:11 +0200

    [TASK] add extension point to institution

--- Classes/Controller/InstitutionController.php
+++ Classes/Controller/InstitutionController.php
@@ -2,10 +2,12 @@
 
 namespace KayStrobach\Contact\Controller;
 
+use KayStrobach\Contact\Domain\Model\User;
 use KayStrobach\Contact\Domain\Repository\UserRepository;
 use Neos\Flow\Annotations as Flow;
 use KayStrobach\Contact\Domain\Model\Institution;
 use KayStrobach\Contact\Domain\Repository\InstitutionRepository;
+use Neos\Flow\Mvc\View\ViewInterface;
 
 class InstitutionController extends \Neos\Flow\Mvc\Controller\ActionController
 {
@@ -61,10 +63,27 @@ class InstitutionController extends \Neos\Flow\Mvc\Controller\ActionController
     public function editAction(Institution $institution) {
         $this->view->assign('institution', $institution);
         $this->view->assign('users', $this->userRepository->findByInstitution($institution));
+
+        $partials = [];
+        $data = [];
+        $this->emitRenderSettings($this->view, $institution, $partials, $data);
+        $this->view->assign('additionalPartials', $partials);
+        $this->view->assignMultiple($data);
     }
 
     public function updateAction(Institution $institution) {
         $this->institutionRepository->update($institution);
         $this->redirect('index');
     }
+
+    /**
+     * @param ViewInterface $view
+     * @param Institution $institution
+     * @param array $additionalPartials
+     * @param array $additionalPartialsData
+     * @return void
+     * @Flow\Signal
+     */
+    protected function emitRenderSettings(ViewInterface $view, Institution $institution, &$additionalPartials, &$additionalPartialsData)
+    {}
 }
